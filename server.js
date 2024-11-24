7b3003264460a589a646eeeb76347aebdf4aad18
DatHuynhCoder <htdat2004@gmail.com>
2024-11-24 11:28:22 +0700

    Khoi tao backend cho WinEng

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+
+const app = express();
+app.use(cors());
+
+//Kết nối tới database crud trên MySQL mở bằng Xampp
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'wineng_db'
+})
+
+//Xử lý yêu cầu get của React
+app.get('/', (req, res) => {
+  const sql = "SELECT * FROM question_bank";
+  db.query(sql, (err, result) => {
+    if(err) return res.json({Message: 'Error inside server'});
+    else res.json(result);
+  })
+})
+//Mở sever express ở port 8081
+app.listen(8081, () => {
+  console.log('Listening me server, please wake up, give me hope');
+})
